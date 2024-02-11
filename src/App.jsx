8ad87a6fc65b4ebc0378457377412356c85f4f05
Dorian <dorianoriginaldesings@gmail.com>
2024-02-11 16:02:57 +0100

    Start

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
+import { GlobalStyles } from './styles/GlobalStyles';
+
 const App = () => {
-	return <h1>Nucleo de la aplicación</h1>;
+	return (
+		<>
+			<GlobalStyles />
+			<h1>Nucleo de la aplicación</h1>
+		</>
+	);
 };
 
 export default App;
---
+++ src/styles/GlobalStyles.js
@@ -0,0 +1,34 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyles = createGlobalStyle`
+   *,
+  *::after,
+  *::before{
+    box-sizing: border-box;
+  }
+
+  img{
+    display: block;
+    max-width: 100%;
+  }
+  
+  body{
+    margin: 0;
+    font-family: sans-serif;
+    background-color: #333;
+    color: aliceblue;
+    font-family: 'Roboto', sans-serif;
+  }
+
+  a{
+    text-decoration: none;
+    color: inherit;
+  }
+
+  ul{
+    list-style: none;
+    margin-top: 0;
+    margin-bottom: 0;
+    padding-left: 0;
+  }  
+`;
