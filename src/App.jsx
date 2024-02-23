75c31a3385d952055837bf1ffbd04c1ca7b9f1ae
Dorian <35946850+DorianDesings@users.noreply.github.com>
2024-02-23 08:52:16 +0100

    fix navigate

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Navigate } from 'react-router-dom';
 import Router from './router/Router';
 import { GlobalStyles } from './styles/GlobalStyles';
 
